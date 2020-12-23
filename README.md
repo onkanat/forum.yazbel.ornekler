a72be02d8b7e86c6f439b6e8f7ead3cd156cab68
onkanat <47117474+onkanat@users.noreply.github.com>
2020-12-23 16:17:28 +0300

    desktop
    desktop deneme

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # forum.yazbel.ornekler
-#### YazBel Forumda karşılaşılan sorular üzerine yapılan pratikler.
+
+## YazBel Forumda karşılaşılan sorular üzerine yapılan pratikler
 
 Forum okumaları sırasında Markdown yazı formatını ve github **READE.md** dosyasının bu formatla yazıldığını öğrendim.
 
+### Yapılacak İşler
 
-### Yapılacak İşler:
-
-- [ ] Sayfa başına *logo* koy.    
+- [ ] Sayfa başına *logo* koy.
 - [ ] Tahmin programını github'a koy.
 - [ ] Sqlite çalışma notlarını düzenle.
 - [ ] _Ağırlık hesaplama kütüphanesini düzelt._
---
+++ tahmin.py
@@ -0,0 +1,52 @@
+### Bu kod https://forum.yazbel.com/t/python-tahmin-oyununda-kod-duzeltmesi/10528/3 alınmıştır.
+import time
+import random
+
+print("""*******
+
+1, ile 40 arasındaki sayiyi tahmin edelim.
+
+*********\n""")
+
+rastgele_sayi = random.randint(1,40) # 1 ile 40 arasında rastgele int sayı belirler.
+
+tahmin_hakkı=5 # Kullanıcı için tahmin hakkı
+
+while True:
+
+    tahmin=int(input("tahmininiz:")) 
+
+    if (tahmin < rastgele_sayi): # Tahmin sayıdan küçükse burası çalışır.
+        tahmin_hakkı-=1
+        
+        if (tahmin_hakkı)<1:
+            print(" üzgünüm hakkınız bitti")
+            print("sayımız:",rastgele_sayi)
+            break
+        else:
+            print("bilgiler sorgulanıyor....")
+            time.sleep(1)
+            print("daha yüksek bir sayi söyleyin...")
+            
+            print(tahmin_hakkı)
+
+
+            
+    elif(tahmin > rastgele_sayi): # Tahmin sayıdan büyükse burası çalışır.
+        tahmin_hakkı-=1
+         
+        if (tahmin_hakkı)<1:
+            print(" üzgünüm hakkınız bitti")
+            print("sayımız:",rastgele_sayi)
+            break
+        else:
+             print("bilgiler sorgulanıyor....")
+             time.sleep(1)
+             print("daha düşük bir sayi söyleyin...")
+             
+             print(tahmin_hakkı)
+    else:                               # Büyük yada Küçük değil ise burası çalışır.
+        print("bilgiler sogulanıyor...")
+        time.sleep(1)
+        print("tebrikler! sayimiz",rastgele_sayi)
+        break
