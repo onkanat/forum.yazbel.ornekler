2dcb4f47008779232ec06d30eab13953d87a1c32
onkanat <47117474+onkanat@users.noreply.github.com>
2020-12-23 19:23:32 +0300

    Desktop
    Sadeleştirilmiş son hal

--- tahmin.py
+++ tahmin.py
@@ -9,40 +9,32 @@ print("""*******
 *********\n""")
 
 rastgele_sayi = random.randint(1,40) # 1 ile 40 arasında rastgele int sayı belirler.
-tahmin_hakkı=5 # Kullanıcı için tahmin hakkı
+tahmin_hakki=5 # Kullanıcı için tahmin hakkı
 
-while True:
+while tahmin_hakki > 0:
 
-    print("Kalan tahmin hakkınız {}".format(tahmin_hakkı),end="\n"*2)
-    tahmin=int(input("tahmininiz:")) 
+    print("Kalan tahmin hakkınız {}".format(tahmin_hakki),end="\n"*2)
+    tahmin=int(input("tahmininiz:"))
+    tahmin_hakki-=1 
 
     if (tahmin < rastgele_sayi): # Tahmin sayıdan küçükse burası çalışır.
-        tahmin_hakkı-=1
         
-        if (tahmin_hakkı)<1:
-            print(" üzgünüm hakkınız bitti")
-            print("sayımız:",rastgele_sayi)
-            break
-        else:
-            print("bilgiler sorgulanıyor....")
-            time.sleep(1)
-            print("daha yüksek bir sayi söyleyin...")
-            print(tahmin_hakkı)
-            
+        print("bilgiler sorgulanıyor....")
+        time.sleep(1)
+        print("daha YÜKSEK bir sayi söyleyin...")
+                        
     elif(tahmin > rastgele_sayi): # Tahmin sayıdan büyükse burası çalışır.
-        tahmin_hakkı-=1
-         
-        if (tahmin_hakkı)<1:
-            print(" üzgünüm hakkınız bitti")
-            print("sayımız:",rastgele_sayi)
-            break
-        else:
-             print("bilgiler sorgulanıyor....")
-             time.sleep(1)
-             print("daha düşük bir sayi söyleyin...")             
-             print(tahmin_hakkı)
+        
+        print("bilgiler sorgulanıyor....")
+        time.sleep(1)
+        print("daha DÜŞÜK bir sayi söyleyin...")             
+             
     else:                               # Büyük yada Küçük değil ise burası çalışır.
         print("bilgiler sogulanıyor...")
         time.sleep(1)
         print("tebrikler! sayimiz",rastgele_sayi)
         break
+
+else:
+    print(" üzgünüm hakkınız bitti")
+    print("sayımız:",rastgele_sayi)
